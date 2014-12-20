85ad652c977948936f6d96cf0df5f75b987c244b
Idan Goldman <idan@goldman.work>
2014-12-21 00:06:12 +0200

    now we can clean completed tasks :)

--- app/list/view.js
+++ app/list/view.js
@@ -3,12 +3,31 @@ define('ListView', ['underscore', 'backbone', 'ListItemView', 'text!templates/li
         tagName: 'section',
         className: 'tasks',
         template: _.template(Template),
+        events: {
+            'click .clean-list button': 'cleanCompleteModels'
+        },
+        showCleanListBotton: function(bool) {
+            this.$el.toggleClass('show-clean-list-button', bool);
+        },
+        cleanCompleteModels: function(event) {
+            this.collection.cleanCompleted();
+
+            event.preventDefault();
+        },
         render: function(collection) {
+            var completeModel = false;
+
             this.$el.empty().append(this.template);
 
             this.$('ul').append(_.map(collection, function(model) {
+                if (model.get('complete')) {
+                    completeModel = true;
+                }
+
                 return new ListItemView({model: model}).el;
             }));
+
+            this.showCleanListBotton(completeModel);
 
             return this;
         }
