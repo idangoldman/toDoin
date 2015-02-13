2a179f71a1345da4e08700a9364bacfcaf06ec01
Idan Goldman <idan@goldman.work>
2015-02-14 01:55:43 +0200

    well we need to start testing at some point in this project, why not to start from the rtl-support feature

---
+++ tests/directionSpec.js
@@ -0,0 +1,20 @@
+var config = require('../settings/config'),
+    direction = require(config.path.app + '/utility/direction');
+
+describe("Direction", function() {
+    it("Should be direction RTL", function() {
+        var text = '';
+
+        expect(direction.is(text)).toBe('rtl');
+        expect(direction.is_rtl(text)).toBeTruthy();
+        expect(direction.is_ltr(text)).toBeFalsy();
+    });
+
+    it("Should be direction LTR", function() {
+        var text = '';
+
+        expect(direction.is(text)).toBe('ltr');
+        expect(direction.is_ltr(text)).toBeTruthy();
+        expect(direction.is_rtl(text)).toBeFalsy();
+    });
+});
