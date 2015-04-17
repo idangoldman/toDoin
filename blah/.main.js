f3c8258d25ce7210bfcedf2941249bc664ed72bd
Idan Goldman <idan@goldman.work>
2015-04-17 22:06:08 +0300

    blah now have general html output function

--- blah/.main.js
+++ blah/.main.js
@@ -4,6 +4,14 @@ var Email = require('./email'),
     Url = require('./url');
 
 module.exports = {
-    email: new Email(),
-    url: new Url()
+    email: Email,
+    url: Url,
+    html: function(string) {
+        if (string.length) {
+            string = this.email.html(string);
+            string = this.url.html(string);
+        }
+
+        return string;
+    }
 };
