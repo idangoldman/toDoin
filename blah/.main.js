8fb4ece22149a9167355a8ee21f6983297185e9f
Idan Goldman <idan@goldman.work>
2015-04-18 00:06:47 +0300

    last thing, main file got smaller and cleaner

--- blah/.main.js
+++ blah/.main.js
@@ -1,17 +1,7 @@
 'use strict';
 
-var Email = require('./email'),
-    Url = require('./url');
+var html = require('./html');
 
 module.exports = {
-    email: Email,
-    url: Url,
-    html: function(string) {
-        if (string.length) {
-            string = this.email.html(string);
-            string = this.url.html(string);
-        }
-
-        return string;
-    }
+    html: html
 };
