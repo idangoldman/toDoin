44b7d13605eaa175028c5bddcef1d8c144960a9e
Idan Goldman <idan@goldman.work>
2015-04-17 20:37:17 +0300

    url class got a check up face lift. still need to change some stuff to make it work.

--- blah/url.js
+++ blah/url.js
@@ -1,8 +1,27 @@
 'use strict';
 
-var base = require('./base');
+var Base = require('./base'),
+    _ = require('underscore'),
 
-var expression = new RegExp(/^((https?:)?\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/, 'g'),
-    url = new base(expression);
+    Url = Base.extend({
+        init: function() {
+            this.expression = "((https?:)?\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?";
+        }
+    });
 
-module.exports = url;
+Url.prototype.html = function(string) {
+    var urls = this.match(string);
+
+    if (urls.length) {
+        _.each(urls, function(url) {
+            var title = url,
+                urlTag = ['<a href="', url, '" class="url">', title, '</a>'].join('');
+
+            string = string.replace(url, urlTag);
+        });
+    }
+
+    return string;
+};
+
+module.exports = Url;
