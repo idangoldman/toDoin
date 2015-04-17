3d4aeafb7a0d9bac9807f5c70502dcf4f84ea9fc
Idan Goldman <idan@goldman.work>
2015-04-18 00:05:21 +0300

    tests fixing because of the change of folders

--- tests/emailSpec.js
+++ tests/emailSpec.js
@@ -1,31 +1,31 @@
 var config = require('../settings/config'),
-    Blah = require(config.path.app + '/../blah/.main');
+    Email = require(config.path.app + '/../blah/helpers/email');
 
 describe("Email validation check", function() {
     it("Should be a valid email", function() {
-        expect(Blah.email.is('idan@idan.com')).toBeTruthy();
-        expect(Blah.email.is('id.an@idan.com')).toBeTruthy();
-        expect(Blah.email.is('id-an@idan.com')).toBeTruthy();
+        expect(Email.is('idan@idan.com')).toBeTruthy();
+        expect(Email.is('id.an@idan.com')).toBeTruthy();
+        expect(Email.is('id-an@idan.com')).toBeTruthy();
     });
 
     it("Should not be a valid email", function() {
-        expect(Blah.email.is('ida@n@idan.com')).toBeFalsy();
-        expect(Blah.email.is('id.an@id&an.com')).toBeFalsy();
-        expect(Blah.email.is('id-an@ida*n.com')).toBeFalsy();
+        expect(Email.is('ida@n@idan.com')).toBeFalsy();
+        expect(Email.is('id.an@id&an.com')).toBeFalsy();
+        expect(Email.is('id-an@ida*n.com')).toBeFalsy();
     });
 });
 
 describe("Email html tag check", function() {
     it("Should be a valid email link", function() {
-        expect(Blah.email.html('idan@idan.com')).toBe('<a href="mailto:idan@idan.com" target="_blank">idan</a>');
-        expect(Blah.email.html('some thing to say: idan@idan.com')).toBe('some thing to say: <a href="mailto:idan@idan.com" target="_blank">idan</a>');
-        expect(Blah.email.html('idan@idan.com <- my old email.')).toBe('<a href="mailto:idan@idan.com" target="_blank">idan</a> <- my old email.');
-        expect(Blah.email.html('idan@idan.com <- two emails -> leeya@leeya.com')).toBe('<a href="mailto:idan@idan.com" target="_blank">idan</a> <- two emails -> <a href="mailto:leeya@leeya.com" target="_blank">leeya</a>');
+        expect(Email.html('idan@idan.com')).toBe('<a href="mailto:idan@idan.com" target="_blank">@idan</a>');
+        expect(Email.html('some thing to say: idan@idan.com')).toBe('some thing to say: <a href="mailto:idan@idan.com" target="_blank">@idan</a>');
+        expect(Email.html('idan@idan.com <- my old email.')).toBe('<a href="mailto:idan@idan.com" target="_blank">@idan</a> <- my old email.');
+        expect(Email.html('idan@idan.com <- two emails -> leeya@leeya.com')).toBe('<a href="mailto:idan@idan.com" target="_blank">@idan</a> <- two emails -> <a href="mailto:leeya@leeya.com" target="_blank">@leeya</a>');
     });
 
     it("Should not be a valid email link", function() {
-        expect(Blah.email.html('idan@ida(n.com')).toBe('idan@ida(n.com');
-        expect(Blah.email.html('not an email -> idan@ida(n.com')).toBe('not an email -> idan@ida(n.com');
-        expect(Blah.email.html('idan@ida%n.com <- another not email')).toBe('idan@ida%n.com <- another not email');
+        expect(Email.html('idan@ida(n.com')).toBe('idan@ida(n.com');
+        expect(Email.html('not an email -> idan@ida(n.com')).toBe('not an email -> idan@ida(n.com');
+        expect(Email.html('idan@ida%n.com <- another not email')).toBe('idan@ida%n.com <- another not email');
     });
 });
--- tests/urlSpec.js
+++ tests/urlSpec.js
@@ -1,32 +1,32 @@
 var config = require('../settings/config'),
-    Blah = require(config.path.app + '/../blah/.main');
+    Url = require(config.path.app + '/../blah/helpers/url');
 
 describe("Url validation check", function() {
     it("Should be a valid url", function() {
-        expect(Blah.url.is('http://idan.co.il')).toBeTruthy();
-        expect(Blah.url.is('www.idan.com/#sdfsadf')).toBeTruthy();
-        expect(Blah.url.is('http://leeya.com')).toBeTruthy();
-        expect(Blah.url.is('http://leeya.com/?blah')).toBeTruthy();
+        expect(Url.is('http://idan.co.il')).toBeTruthy();
+        expect(Url.is('www.idan.com/#sdfsadf')).toBeTruthy();
+        expect(Url.is('http://leeya.com')).toBeTruthy();
+        expect(Url.is('http://leeya.com/?blah')).toBeTruthy();
     });
 
     it("Should not be a valid url", function() {
-        expect(Blah.url.is('idan.com')).toBeFalsy();
-        expect(Blah.url.is('http://blah.$.com')).toBeFalsy();
-        expect(Blah.url.is('www.lee$ya.com')).toBeFalsy();
+        expect(Url.is('idan.com')).toBeFalsy();
+        expect(Url.is('http://blah.$.com')).toBeFalsy();
+        expect(Url.is('www.lee$ya.com')).toBeFalsy();
     });
 });
 
 describe("Url html tag check", function() {
     it("Should be a valid url link", function() {
-        expect(Blah.url.html('http://idan.com')).toBe('<a href="http://idan.com" target="_blank">idan.com</a>');
-        expect(Blah.url.html('some thing to say: www.idan.com')).toBe('some thing to say: <a href="http://www.idan.com" target="_blank">idan.com</a>');
-        expect(Blah.url.html('http://blah.co.il <- my old url.')).toBe('<a href="http://blah.co.il" target="_blank">blah.co.il</a> <- my old url.');
-        expect(Blah.url.html('http://idan.com <- two urls -> http://leeya.com')).toBe('<a href="http://idan.com" target="_blank">idan.com</a> <- two urls -> <a href="http://leeya.com" target="_blank">leeya.com</a>');
+        expect(Url.html('http://idan.com')).toBe('<a href="http://idan.com" target="_blank">idan.com</a>');
+        expect(Url.html('some thing to say: www.idan.com')).toBe('some thing to say: <a href="http://www.idan.com" target="_blank">idan.com</a>');
+        expect(Url.html('http://blah.co.il <- my old url.')).toBe('<a href="http://blah.co.il" target="_blank">blah.co.il</a> <- my old url.');
+        expect(Url.html('http://idan.com <- two urls -> http://leeya.com')).toBe('<a href="http://idan.com" target="_blank">idan.com</a> <- two urls -> <a href="http://leeya.com" target="_blank">leeya.com</a>');
     });
 
     it("Should not be a valid url link", function() {
-        expect(Blah.url.html('http://ida(n.com')).toBe('http://ida(n.com');
-        expect(Blah.url.html('not an url -> http://ida(n.com')).toBe('not an url -> http://ida(n.com');
-        expect(Blah.url.html('www.ida%n.com <- another not url')).toBe('www.ida%n.com <- another not url');
+        expect(Url.html('http://ida(n.com')).toBe('http://ida(n.com');
+        expect(Url.html('not an url -> http://ida(n.com')).toBe('not an url -> http://ida(n.com');
+        expect(Url.html('www.ida%n.com <- another not url')).toBe('www.ida%n.com <- another not url');
     });
 });
