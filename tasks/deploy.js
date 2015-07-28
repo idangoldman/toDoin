0bc69f4cd06b5c670074274a0ef0d77e25e3797d
Idan Goldman <idan@goldman.work>
2015-07-28 23:13:34 +0300

    page can be uploaded by page-deploy task after building the page

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp'),
     ftp = require('gulp-ftp'),
+    ssh = require('gulp-ssh'),
+    fs = require('fs'),
     gutil = require('gulp-util'),
 
     auth = require('../settings/auth.json');
@@ -12,5 +14,21 @@ gulp.task('ftp-deploy', function () {
             pass: auth.ftp.pass,
             remotePath: auth.ftp.path
         }))
+        .pipe(gutil.noop());
+});
+
+gulp.task('page-deploy', function () {
+    var pageSSH = new ssh({
+      ignoreErrors: false,
+      sshConfig: {
+          host: auth.ssh.host,
+          port: auth.ssh.port,
+          username: auth.ssh.username,
+          privateKey: fs.readFileSync( auth.ssh.privateKey )
+        }
+    })
+
+    return gulp.src('./_build/**/*')
+        .pipe(pageSSH.dest( auth.ssh.path.page ))
         .pipe(gutil.noop());
 });
