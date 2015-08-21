50250f96638626b52675e4cb686b4ecf5f820250
Idan Goldman <idan@goldman.work>
2015-08-21 21:19:23 +0300

    version bump

--- app/header/view.js
+++ app/header/view.js
@@ -20,7 +20,7 @@ module.exports = Backbone.View.extend({
             title: 'Sort by date',
             selected: false
         }],
-        version: '2.0.0'
+        version: '2.1.0'
     },
     initialize: function() {
         this.listenTo(this.collection, 'remove', this.render);
