3ce41c4f6c783165b2a225fbd0743a1ffdb4a864
Idan Goldman <idan@goldman.work>
2016-02-16 23:20:58 +0200

    "eating stuff" as clearing tasks is back! :D

--- app/clean-button/view.js
+++ app/clean-button/view.js
@@ -8,13 +8,37 @@ module.exports = Backbone.View.extend({
         'type': 'button',
     },
     className: 'clean-button',
+    events: {
+        'click': 'cleanCompleteModels'
+    },
+    eatText: [{
+        one: 'Cookie',
+        many: 'Cookies'
+    },
+    {
+        one: 'Walnut',
+        many: 'Walnuts'
+    },
+    {
+        one: 'Apple',
+        many: 'Apples'
+    },
+    {
+        one: 'Cucumber',
+        many: 'Cucumbers'
+    },
+    {
+        one: 'Banana',
+        many: 'Bananas'
+    },
+    {
+        one: 'Tomato',
+        many: 'Tomatoes'
+    }],
     initialize: function() {
         this.render();
         this.listenTo(this.collection, 'change:complete', this.render);
         Utility.vent.on('typing:adjust-height', this.adjustHeight, this);
-    },
-    events: {
-        'click': 'cleanCompleteModels'
     },
     adjustHeight: function(toAdjust) {
         this.$el.toggleClass('bottom-double-space', toAdjust);
@@ -28,7 +52,20 @@ module.exports = Backbone.View.extend({
         this.$el.toggleClass('show', !!this.collection.completeCount);
     },
     pickButtonText: function(completeCount) {
-        return 'Clear Complete ' + (completeCount > 1 ? 'Todos' : 'Todo');
+        var eatText = _.sample(this.eatText),
+            buttonText = '',
+            noun = 'a';
+
+        if (completeCount > 1) {
+            buttonText = ['Eat', completeCount,  eatText.many + '.'].join(' ');
+        } else {
+            if (eatText.one === 'Apple') {
+                noun = 'an';
+            }
+
+            buttonText = ['Eat', noun, eatText.one + '.'].join(' ');
+        }
+        return buttonText;
     },
     render: function() {
         var completeCount = this.collection.completeCount,
