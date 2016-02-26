ab6b11f438b007a0774c1ed0b96caf5f7780f609
Idan Goldman <idan@goldman.work>
2016-02-26 03:14:01 +0200

    sort by menu functioning and looking good! :)

--- app/header/view.js
+++ app/header/view.js
@@ -1,5 +1,6 @@
 var Backbone = require('backbone'),
     _ = require('underscore'),
+    $ = require('jquery'),
     Template = require('./template.html');
 
 module.exports = Backbone.View.extend({
@@ -21,16 +22,27 @@ module.exports = Backbone.View.extend({
         version: '2.3.1'
     },
     events: {
-        'click .sort-by': 'open',
+        'click .sort-by': 'openSortBy',
         'click .sort-by a': 'navigate'
     },
     initialize: function() {
         this.listenTo(this.collection, 'remove', this.render);
         this.listenTo(this.collection, 'change', this.render);
         Backbone.history.on('route', this.render, this);
+        this.closeSortBy = this.closeSortBy.bind(this);
+        $el = this.$el;
     },
-    open: function(event) {
+    closeSortBy: function(event) {
+        if(!$(event.target).closest('.sort-by').length && !$(event.target).is('.sort-by')) {
+            if(this.$el.find('.sort-by').hasClass('open')) {
+                this.$el.find('.sort-by').removeClass('open');
+                $('body').off('click', this.closeSortBy);
+            }
+        }
+    },
+    openSortBy: function(event) {
         this.$el.find('.sort-by').toggleClass('open');
+        $('body').on('click', this.closeSortBy);
         event.preventDefault();
     },
     navigate: function(event) {
