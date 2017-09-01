8fe6ffd0cd0a51ed70e29cd6bccc5bfd17432fb7
Idan Goldman <idan@goldman.work>
2017-09-02 00:11:56 +0300

    info.js, cleanups

---
+++ info.example.js
@@ -0,0 +1,16 @@
+import PACKAGE_INFO from 'package';
+
+
+export const INFO = {
+    name: PACKAGE_INFO.name,
+    author: PACKAGE_INFO.author,
+    version: PACKAGE_INFO.version,
+    license: PACKAGE_INFO.license,
+
+    extension: {
+        id: '',
+        url: ''
+    },
+
+    googleAnalyticsId: ''
+};
